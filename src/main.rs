fb9db05cdfa4d7d9a09d608f47387098e9ff5074
Leandro <leanoaks@gmail.com>
2021-12-05 00:01:10 -0300

    initial debugging done, more tomorrow

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,8 @@ fn get_word(file_path: &str) -> Option<String> {
     word
 }
 
-fn get_sprites() -> (String, String, String, String, String, String, String) {
-    (
+fn get_sprites() -> [String; 7] {
+    [
         "_____\n|   |\n|   O\n|  /|\\\n|  / \\\n|\n|".to_string(),
         "_____\n|   |\n|   O\n|  /|\\\n|  /\n|\n|".to_string(),
         "_____\n|   |\n|   O\n|  /|\\\n|\n|\n|".to_string(),
@@ -27,63 +27,67 @@ fn get_sprites() -> (String, String, String, String, String, String, String) {
         "_____\n|   |\n|   O\n|  /\n|\n|\n|".to_string(),
         "_____\n|   |\n|   O\n|\n|\n|\n|".to_string(),
         "_____\n|   |\n|\n|\n|\n|\n|".to_string()
-    )
+    ]
 }
 
 fn get_secret_letters(word :&String) -> HashSet<char> {
     let mut letters = HashSet::new();
-    for l in word {
-        letters.insert(l as char);
+    for l in word.chars() {
+        letters.insert(l.clone());
     };
     letters
 }
 
 fn secret_word(word :&String, secret_letters :&HashSet<char>) -> String {
     let mut secret_word = String::new();
-    for l in word {
-        if secret_letters.contains(l) {
+    for l in word.chars() {
+        if secret_letters.contains(&l) {
             secret_word.push('_');
         } else {
-            secret_word.push(l);
+            secret_word.push(l.clone());
         };
     };
     secret_word
 }
 
-fn prompt(guesses: &mut Vec<String>) -> String {
+fn prompt(guesses: &mut Vec<char>) -> char {
     let mut buffer = String::new();
-    let mut stdin = io::stdin();
+    let stdin = io::stdin();
     print!("enter a guess: ");
     match stdin.read_line(&mut buffer) {
-        Ok(i) => {
-            guesses.push(i);
-            i
+        Ok(_i) => {
+            let input = buffer.remove(0);
+            guesses.push(input.clone());
+            input
         },
-        Err(_) => String::new(),
+        Err(_) => ' ',
     }
 }
 
-fn process_guessing(guess :&String, secret_letters :&mut HashSet<char>) {
-    match secret_letters.remove(&guess) {
-        true => println!("{} is a secret word", guess);
-        false => println!("{} is not a secret word", guess);
+fn process_guessing(guess :&char, tries :&mut usize, secret_letters :&mut HashSet<char>) {
+    match secret_letters.remove(guess) {
+        true => println!("{} is a secret word", guess),
+        false => {
+            println!("{} is not a secret word", guess);
+            tries -= 1;
+        }
     };
 }
 
-fn game(word: mut String) {
+fn game(word: String) {
     let mut tries = 6;
     let sprites = get_sprites();
-    let mut guess = String::new();
-    let mut guesses = Vec::new();
+    let mut guess: char;
+    let mut guesses :Vec<char> = Vec::new();
     let mut secret_letters = get_secret_letters(&word);
-    while assert_ne!(tries, 0) && assert!(!secret_letters.is_empty()) {
+    while tries != 0 && !secret_letters.is_empty() {
         println!("Guesses: {:?}", guesses);
-        println!("{}", sprites.tries);
+        println!("{}", sprites[tries]);
         println!("    {}", secret_word(&word, &secret_letters));
-        guess = prompt(&guesses);
-        process_guessing(&guess, &mut secret_letters);
+        guess = prompt(&mut guesses);
+        process_guessing(&guess, &mut tries, &mut secret_letters);
     };
-    if assert_eq!(tries, 0) || assert!(secret_letters.is_empty()) {
+    if tries == 0 || secret_letters.is_empty() {
         println!("You lost");
     } else {
         println!("You Won");
