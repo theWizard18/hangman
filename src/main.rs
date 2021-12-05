828145bfaa48ac25efb8e0ef4cedd2febbd33179
Leandro <leanoaks@gmail.com>
2021-12-04 21:08:40 -0300

    added hangman ascii arts, the guesses vector, the secret_letters hashset, among other things

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use std::collections::HashSet;
 use std::fs;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 
-fn get_secret_word(file_path: &str) -> Option<String> {
+fn get_word(file_path: &str) -> Option<String> {
     let content = match fs::read_to_string(file_path) {
         Ok(file) => file,
         Err(_) => String::new(),
@@ -16,9 +17,40 @@ fn get_secret_word(file_path: &str) -> Option<String> {
     word
 }
 
+fn get_secret_letters(word :&String) -> HashSet<String> {
+    let mut letters = HashSet::new();
+    for l in word {
+        letters.insert(l.to_string());
+    };
+    letters
+}
+
+fn get_sprites() -> (String, String, String, String, String, String, String) {
+    (
+        "_____\n|   |\n|   O\n|  /|\\\n|  / \\\n|\n|".to_string(),
+        "_____\n|   |\n|   O\n|  /|\\\n|  /\n|\n|".to_string(),
+        "_____\n|   |\n|   O\n|  /|\\\n|\n|\n|".to_string(),
+        "_____\n|   |\n|   O\n|  /|\n|\n|\n|".to_string(),
+        "_____\n|   |\n|   O\n|  /\n|\n|\n|".to_string(),
+        "_____\n|   |\n|   O\n|\n|\n|\n|".to_string(),
+        "_____\n|   |\n|\n|\n|\n|\n|".to_string()
+    )
+}
+        
+fn game(word: mut String) {
+    let mut tries = 6;
+    let sprites = get_sprites();
+    let mut guesses = Vec::new()
+    let mut secret_letters = get_secret_letters(&word);
+    while assert_ne!(tries, 0) && assert!(!secret_letters.is_empty()) {
+        println!("Guesses: {:?}", guesses);
+        println!("{}", sprites.tries);
+    }
+}
+
 fn main() {
-    match get_secret_word("src/words.txt") {
-        Some(secret_word) => println!("{}", secret_word),
+    match get_word("src/words.txt") {
+        Some(word) => println!("{}", word),
         None => println!("file couldn't be found."),
     };
 }
